eaedee190821f7fb2e88a0377fcd3937ab4530fe
Francisco Beltran <roshlandia@gmail.com>
2022-10-16 02:27:30 -0500

    responsive67

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -6,10 +6,16 @@ const Footer = () => {
         <div>
         <h5>App FULL STACK hecha con  ♥  por Francisco Béltran </h5>
         <div className='FooterR'>
-        <img src="https://cdn-icons-png.flaticon.com/512/3670/3670125.png" alt="" />
-        <img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="" />
-        <img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt="" />
-       
+       <a href="https://www.instagram.com/beltranbolibar/"target={'_blank'}>
+       <img src="https://cdn-icons-png.flaticon.com/512/3670/3670125.png"  />
+       </a>
+       <a href="https://github.com/Francisco-Tirador"target={'_blank'}>
+       <img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" />
+       </a>
+       <a href="https://www.linkedin.com/in/francisco-beltr%C3%A1n-342a31213/" target={'_blank'}>
+       <img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"  />
+       </a>
+
         </div>
         </div>
     </div>
